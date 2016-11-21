a634b811ac502651a8e7929bd16fd8464033bb0e
Roman Dudin <roman.dudin@akvelon.com>
2016-11-21 22:16:10 +0300

    Initial commit.

---
+++ UnitTestProject1/Hooks1.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+using System.Threading;
+
+namespace UnitTestProject1
+{
+    [Binding]
+    public sealed class Hooks1
+    {
+        private static ChromeDriver Driver = new ChromeDriver();
+
+        [BeforeTestRun]
+        public static void BeforeTests()
+        {
+            Driver.Navigate().GoToUrl("http://ya.ru");
+        }
+
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            //TODO: implement logic that has to run before executing each scenario
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            //TODO: implement logic that has to run after executing each scenario
+        }
+    }
+}
